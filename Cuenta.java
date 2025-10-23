1e2e6d67d9491a8f9ccdc51920d1867828c9bb2d
PatriciaOM23 <patricia.olmedo.majano23@gmail.com>
2025-10-23 10:17:21 +0200

    Create Cuenta.java
    PROBAR COSAS NUEVAS

---
+++ Cuenta.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Cuenta {
+
+    public static void Devolver(int cantidad) {
+
+        if (cantidad > 0) {
+            System.out.println(cantidad);
+            cantidad--;
+            Devolver(cantidad);
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un número: ");
+        int cantidad = sc.nextInt();
+        Devolver(cantidad);
+
+        sc.close();
+    }
+}
