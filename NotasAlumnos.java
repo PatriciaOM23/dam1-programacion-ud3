a7162c1791ec5ce9cf0e9293c6863d3504366023
PatriciaOM23 <patricia.olmedo.majano23@gmail.com>
2025-11-12 13:11:28 +0100

    Update NotasAlumnos.java

--- NotasAlumnos.java
+++ NotasAlumnos.java
@@ -1,4 +1,30 @@
 public class NotasAlumnos {
+    
+    public static void calcularMedia(int[][] notasAlumnos){
+
+    double[] mediasAlumnos = new double[notasAlumnos.length];
+    for (int i = 0; i < notasAlumnos.length; i++) {
+        int c = 0;
+        double filaMediasAlumnos = 0;
+    
+        for (int j = 0; j < notasAlumnos[i].length; j++) {
+            filaMediasAlumnos += notasAlumnos[i][j];
+            c++;
+        }
+        filaMediasAlumnos /= c;
+        mediasAlumnos[i] = filaMediasAlumnos;
+       
+    
+        System.out.printf("%.2f",mediasAlumnos[i]);
+        if (i < notasAlumnos.length - 1) {
+            System.out.print(", ");
+        }
+    }
+    
+    }
+
+    
+    
     public static void main(String[] args) {
 
         int[][] notasAlumnos = new int[][] {
@@ -8,30 +34,8 @@ public class NotasAlumnos {
                 { 8, 6, 1 },
         };
 
+        calcularMedia(notasAlumnos);
         // Devolver media de cada fila
         // double[] mediasAlumnos = new double[]{4,3.6,4,5};
-
-        double filaMediasAlumnos = 0;
-
-        double[] mediasAlumnos = new double[notasAlumnos.length];
-        int c = 0;
-        for (int i = 0; i < notasAlumnos.length; i++) {
-
-            for (int j = 0; j < notasAlumnos[i].length; j++) {
-                filaMediasAlumnos += notasAlumnos[i][j];
-                c++;
-            }
-            filaMediasAlumnos /= c;
-            mediasAlumnos[i] = filaMediasAlumnos;
-            c = 0;
-            filaMediasAlumnos = 0;
-
-            System.out.printf("%.2f",mediasAlumnos[i]);
-            if (i < notasAlumnos.length - 1) {
-                System.out.print(", ");
-            }
-        }
-
     }
-
 }
